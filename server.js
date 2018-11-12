fd5d723aee841a17a9b4bb58b2ecd19d4b81e250
edelmonaco <edelmonaco@wpi.edu>
2018-11-12 13:35:56 -0500

    Sample site with iframe

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ var server = http.createServer (function (req, res) {
     case '/index.html':
       sendFile(res, 'index.html')
       break
+    case 'styles.css':
+      sendFile(res, 'styles.css')
+      break
     default:
       res.end('404 not found')
   }
